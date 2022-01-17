958b0ccf56a36118b485c3f58a634fb26ae71414
cantstopcoding <jonathan_koss@yahoo.com>
2022-01-16 20:46:14 -0500

    create async test

--- src/App.js
+++ src/App.js
@@ -1,25 +1,36 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import axios from "axios";
 
-function App() {
+const URL = "https://pokeapi.co/api/v2/pokemon/";
+
+const App = () => {
+  const [pokemons, setPokemons] = React.useState([]);
+
+  async function handleFetch(event) {
+    let result;
+
+    result = await axios.get(`${URL}`);
+
+    setPokemons(result.data.results);
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <button type="button" onClick={handleFetch}>
+          Fetch Pokemons
+        </button>
+
+        <ul>
+          {pokemons.map((p, k) => (
+            <li key={k + 1}>
+              <a href={p.url}>{p.name}</a>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </>
   );
-}
+};
 
 export default App;
--- src/App.test.js
+++ src/App.test.js
@@ -1,6 +1,30 @@
-import { render, screen } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
+import axios from "axios";
+import userEvent from "@testing-library/user-event";
 import App from "./App";
 
-it("Renders App Component", () => {
-  render(<App />);
+jest.mock("axios");
+
+describe("App", () => {
+  test("renders learn react link", () => {
+    render(<App />);
+  });
+
+  test("fetches pokemons from API and display them", async () => {
+    const pokemons = [
+      { name: "bulbasaur", url: "https://pokeapi.co/api/v2/pokemon/1/" },
+      { name: "ivysaur", url: "https://pokeapi.co/api/v2/pokemon/2/" },
+    ];
+    const promise = Promise.resolve({ data: { results: pokemons } });
+    debugger;
+    axios.get.mockImplementationOnce(() => promise);
+
+    render(<App />);
+
+    await userEvent.click(screen.getByRole("button"));
+
+    await act(() => promise);
+
+    expect(screen.getAllByRole("listitem")).toHaveLength(2);
+  });
 });
